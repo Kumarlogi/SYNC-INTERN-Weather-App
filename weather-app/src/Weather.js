c1408e38c066bc00210bb347a2a49b778ac6edc8
Logika <logi8550@gmail.com>
2023-11-19 11:21:21 +0530

    api key changed

--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -1,47 +1,64 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from 'react';
+import './WeatherApp.css';
 
 const Weather = () => {
-    const [weatherData, setWeatherData] = useState(null);
-    const [error, setError] = useState('');
-    const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [error, setError] = useState('');
 
-    const API_KEY = 'fc6867746f2948bf99a372d90aa6dd20';
+  const API_KEY = '932b52746f2fc6d49837ad450e771c76';
 
-    const fetchWeatherData = async () => {
-        try {
-            const response = await axios.get(
-              `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-            );
-            setWeatherData(response.data);
-            setError('');
-            } catch (err) {
-                setWeatherData(null);
-                setError('City not found. Please try again.');
-            }
-    };
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        setWeatherData(data);
+        setError('');
+      } else {
+        setWeatherData(null);
+        setError(`Error: ${data.message}`);
+      }
+    } catch (err) {
+      setWeatherData(null);
+      setError('An error occurred. Please try again.');
+    }
+  };
+
   return (
-    <div>
+    <div className="weather-app">
       <h1>Weather App</h1>
-      <input
-        type="text"
-        placeholder="Enter city"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button onClick={fetchWeatherData}>Get Weather</button>
+      <div className="search-form">
+        <input
+          type="text"
+          placeholder="Enter city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+        <button onClick={fetchWeatherData}>Get Weather</button>
+      </div>
 
       {weatherData && (
-        <div>
+        <div className="weather-details">
           <h2>{weatherData.name}, {weatherData.sys.country}</h2>
           <p>Temperature: {weatherData.main.temp} K</p>
           <p>Description: {weatherData.weather[0].description}</p>
         </div>
       )}
 
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p className="error-message">{error}</p>}
     </div>
-  )
-}
+  );
+};
 
 export default Weather;
